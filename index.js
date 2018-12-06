081fe257051c30e0cbf274f105f8774cd1a8b207
Majo Durco <majodurco@gmail.com>
2018-12-06 21:01:35 +0100

    Add first tast

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const path = require('path')
+const xml2js = require('xml2js')
+
+const parser = new xml2js.Parser();
+const content = fs.readFileSync(path.resolve(__dirname, 'structure_released.xml'))
+const result = parser.parseString(content, (err, result) => {
+  const x = preorderNodes(result.ImageNetStructure.synset[0], null)
+  console.log(JSON.stringify(x, null, 2))
+})
+
+function preorderNodes(node, nameFromAbove) {
+  let nameFromRoot = [nameFromAbove, node['$'].words].join(' > ')
+  if(!nameFromAbove) nameFromRoot = node['$'].words // initial call
+  let data = [{
+    name: nameFromRoot,
+    size: 0,
+  }]
+  let [ dataHead ] = data;
+  if ('synset' in node) {
+    dataHead.size += node.synset.length
+    for (let deeperNode of node.synset) {
+      const dataFromBelow = preorderNodes(deeperNode, nameFromRoot)
+      const [ head ] = dataFromBelow
+      data.push(...dataFromBelow)
+      dataHead.size += head.size
+    }
+  }
+  return data
+}
